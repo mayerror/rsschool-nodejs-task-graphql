aa350786a0ffff6040928819ffc6a6bfacdd21c9
Sergey Masiuk <mayerror@gmail.com>
2024-03-04 01:47:44 +0300

    feat: implement connectionts graphql with fastify

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,17 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
+import {
+  createGqlResponseSchema,
+  gqlResponseSchema,
+  rootValue,
+  schema,
+  // rootValue,
+  // schema,
+} from './schemas.js';
 import { graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma, httpErrors } = fastify;
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -12,8 +21,18 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
         200: gqlResponseSchema,
       },
     },
+
     async handler(req) {
-      return {};
+      const query = req.body.query;
+      // const data = await prisma.user.findMany();
+      const res = await graphql({
+        schema,
+        source: `{ ${query} }`,
+        rootValue,
+      });
+
+      return { data: res };
+      // return {};
     },
   });
 };
--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,5 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { buildSchema } from 'graphql';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -17,4 +18,16 @@ export const createGqlResponseSchema = {
       additionalProperties: false,
     },
   ),
+};
+
+export const schema = buildSchema(`
+  type Query {
+    users: String
+  }
+`);
+
+export const rootValue = {
+  users: () => {
+    return 'Hello users!';
+  },
 };
